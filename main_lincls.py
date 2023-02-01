762fa57c27028b777edc28b15b85e1f13e378357
levi3001 <canhhoang30011999@gmail.com>
2023-02-01 14:23:44 +0900

    acc

--- main_lincls.py
+++ main_lincls.py
@@ -330,11 +330,11 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
     batch_time = AverageMeter('Time', ':6.3f')
     data_time = AverageMeter('Data', ':6.3f')
     losses = AverageMeter('Loss', ':.4e')
-    #top1 = AverageMeter('Acc@1', ':6.2f')
-    #top5 = AverageMeter('Acc@5', ':6.2f')
+    top1 = AverageMeter('Acc@1', ':6.2f')
+    top5 = AverageMeter('Acc@5', ':6.2f')
     progress = ProgressMeter(
         len(train_loader),
-        [batch_time, data_time, losses],
+        [batch_time, data_time, losses, top1, top5],
         prefix="Epoch: [{}]".format(epoch))
 
     """
@@ -364,10 +364,10 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         loss = criterion(model(sample,positive), pos_lab)+ criterion(model(sample,negative), neg_lab)
 
         # measure accuracy and record loss
-        #acc1, acc5 = accuracy(output, target, topk=(1, 5))
+        acc1, acc5 = accuracy(output, target, topk=(1, 5))
         losses.update(loss.item(), sample.size(0))
-        #top1.update(acc1[0], images.size(0))
-        #top5.update(acc5[0], images.size(0))
+        top1.update(acc1[0], images.size(0))
+        top5.update(acc5[0], images.size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
