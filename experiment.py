0598da4538be69d81b870089ef55e98359bc015e
Hyungjun Yoon <dbslzhem16@gmail.com>
2023-10-24 15:59:11 +0900

    .

--- experiment.py
+++ experiment.py
@@ -42,39 +42,13 @@ class Experiment:
         self.cfg = cfg
         self.logger = logger
         
-        self.configure_gpus()
         self.configure_random_seed()
     
-    def configure_gpus(self):
-        self.logger.info(f'Setting GPUs: {self.cfg.gpu}')
-        self.gpu = []
-        if self.cfg.gpu == 'all':
-            self.gpu = [0,1,2,3,4,5,6,7]
-        elif isinstance(self.cfg.gpu, int):
-            self.gpu = [self.cfg.gpu]
-        elif isinstance(self.cfg.gpu, list) and len(self.cfg.gpu) > 0:
-            self.gpu = self.cfg.gpu
-        else:
-            self.logger.warning(f'Only GPU training is supported. Setting gpu 0 as default.')
-            self.gpu = [0]
-        os.environ["CUDA_VISIBLE_DEVICES"] = ','.join([str(gpu) for gpu in self.gpu])
-        self.logger.info(f'GPUs {self.gpu} will be used')
-    
     def configure_random_seed(self):
-        cudnn.benchmark = True
         if not self.cfg.seed is None:
             random.seed(self.cfg.seed)
             torch.manual_seed(self.cfg.seed)
             np.random.seed(self.cfg.seed)
-            cudnn.benchmark = False
-            cudnn.deterministic = True
-            torch.cuda.manual_seed_all(self.cfg.seed)
-            
-            self.logger.warning('You have chosen to seed training. ')
-            self.logger.warning('This will turn on the CUDNN deterministic setting, ')
-            self.logger.warning('which can slow down your training considerably! ')
-            self.logger.warning('You may see unexpected behavior when restarting ')
-            self.logger.warning('from checkpoints.')
     
     def run(self):
         # Model creation
