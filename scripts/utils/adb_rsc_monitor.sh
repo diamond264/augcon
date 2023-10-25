d38d86b869dfa463df28258d3fc1cc235fde688e
Hyungjun Yoon <dbslzhem16@gmail.com>
2023-10-25 16:27:57 +0900

    .

--- scripts/utils/adb_rsc_monitor.sh
+++ scripts/utils/adb_rsc_monitor.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-  echo "Usage: $0 <PID> <output_file>"
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <output_file>"
   exit 1
 fi
 
-PID="$1"
-OUTPUT_FILE="$2"
+OUTPUT_FILE="$1"
 
 # Execute the adb shell top command and store the output in the specified file
-adb shell top -b -d 1 -p "$PID" | tee "$OUTPUT_FILE" | grep "$PID"
+adb shell top -b -d 0.5 | grep --line-buffered "python" | tee "$OUTPUT_FILE"
--- scripts/utils/adb_rsc_parse.py
+++ scripts/utils/adb_rsc_parse.py
@@ -3,20 +3,30 @@ import argparse
 import pandas as pd
 from glob import glob
 
-def parse(pid, input, output=None):
+def parse(input, output=None):
     df = pd.DataFrame(columns=['cpu', 'mem'])
     
     with open(input, 'r') as f:
         lines = f.readlines()
     
-    for line in lines:
+    pid = -1
+    accumulated_cpu = 0
+    accumulated_mem = 0
+    for e, line in enumerate(lines):
+        if e == 0: pid = line.strip().split()[0]
         l = line.strip()
-        if l.startswith(str(pid)):
+        if len(l) > 0:
             vals = l.split()
             cpu = float(vals[8].strip())
             mem = float(vals[9].strip())
-            new_df = pd.DataFrame({'cpu': [cpu], 'mem': [mem]})
+            accumulated_cpu += cpu
+            accumulated_mem += mem
+            
+        if l.split()[0] == pid:
+            new_df = pd.DataFrame({'cpu': [accumulated_cpu], 'mem': [accumulated_mem]})
             df = pd.concat([df, new_df], ignore_index=True)
+            accumulated_cpu = 0
+            accumulated_mem = 0
     
     if output is not None:
         df.to_csv(output, index=False)
@@ -28,6 +38,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parse adb rsc output')
     parser.add_argument('-i', '--input', help='Input file')
     parser.add_argument('-o', '--output', help='Output file')
-    parser.add_argument('-p', '--pid', help='PID')
     args = parser.parse_args()
-    parse(args.pid, args.input, args.output)
+    parse(args.input, args.output)
