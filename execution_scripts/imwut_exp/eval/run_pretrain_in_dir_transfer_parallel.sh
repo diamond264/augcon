6ef24f4c4d40c1fa25102f26ce69fbae930b0af7
Hyungjun Yoon <dbslzhem16@gmail.com>
2023-11-02 14:16:13 +0900

    .

---
+++ execution_scripts/imwut_exp/eval/run_pretrain_in_dir_transfer_parallel.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if the config directory argument is provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <pretext>"
+  exit 1
+fi
+
+# Directory containing your YAML files
+pretext="$1"
+
+# Loop through each .yaml file in the directory
+for config_file in /mnt/sting/hjyoon/projects/aaa/configs/imwut/main_eval/*/"$pretext"/pretrain_transfer/*.yaml; do
+  # Check if the file exists
+  if [ -e "$config_file" ]; then
+    # Run your Python script with the config file as an argument serially
+    ./experiment.py --config "$config_file" &
+  fi
+done
+
+wait
