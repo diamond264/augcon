df95b022d0c5b93fd40d7ca05e48ba85c84037c7
Hyungjun Yoon <dbslzhem16@gmail.com>
2023-11-03 16:31:23 +0900

    .

--- execution_scripts/imwut_exp/eval/run_finetune_in_dir_eval_serial.sh
+++ execution_scripts/imwut_exp/eval/run_finetune_in_dir_eval_serial.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Check if the config directory argument is provided
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <pretext> <gpu>"
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <pretext> <dataset> <seed>"
   exit 1
 fi
 
 # Directory containing your YAML files
 pretext="$1"
-gpu="$2"
+dataset="$2"
+seed="$3"
 
 # Loop through each .yaml file in the directory
-for config_file in /mnt/sting/hjyoon/projects/aaa/configs/imwut/main_eval/ichar/"$pretext"/finetune/10shot/linear/"$gpu"/*.yaml; do
+for config_file in /mnt/sting/hjyoon/projects/aaa/configs/imwut/main_eval/"$dataset"/"$pretext"/finetune/10shot/linear/"$seed"/*.yaml; do
   # Check if the file exists
   if [ -e "$config_file" ]; then
     # Run your Python script with the config file as an argument serially
