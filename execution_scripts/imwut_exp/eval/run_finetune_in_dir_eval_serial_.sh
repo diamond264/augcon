cb5a1334f9ebebe349e000b57e7668fa3fce88b1
Hyungjun Yoon <dbslzhem16@gmail.com>
2023-11-04 16:03:02 +0900

    .

---
+++ execution_scripts/imwut_exp/eval/run_finetune_in_dir_eval_serial_.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if the config directory argument is provided
+if [ $# -ne 4 ]; then
+  echo "Usage: $0 <pretext> <setting> <seed> <gpu>"
+  exit 1
+fi
+
+# Directory containing your YAML files
+pretext="$1"
+setting="$2"
+seed="$3"
+gpu="$4"
+
+# Loop through each .yaml file in the directory
+for i in {0..7}; do
+  ./execution_scripts/imwut_exp/eval/run_finetune_in_dir_eval_serial.sh "$pretext" "$setting" "$seed" gpu$i &
+done
+
+wait
